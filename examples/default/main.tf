6754f4f4cde507364252aa94cb342722ccba54e9
Andrew Webster <webstean@gmail.com>
2025-07-20 18:35:42 +1000

    Initial/Update example file

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -6,6 +6,37 @@ azuread_group "cloud_operators" {
   security_enabled = true
 }
 
+module "application-landing-zone-setup" {
+  source  = "webstean/application-landing-zone-setup/azurerm"
+  version = "~>0.0, < 1.0"
+
+  ## Identity
+  entra_group_id    = data.azuread_group.cloud_operators.id
+
+  ## Naming
+  landing_zone_name = "play"
+  dns_zone_name     = format("%s.lz.%s", var.landing_zone_name, data.azuread_domains.default.domains[0].domain_name)
+
+  ## Location
+  subscription_id   = data.azurerm_client_config.current.subscription_id
+  location_key      = "australiaeast"                 ## other options are: australiasoutheast, australiacentral
+
+  ## Sizing
+  sku_name          = "free"                          ## other options are: basic, standard, premium or isolated
+  size_name         = "small"                         ## other options are: medium, large or x-large
+
+  ## Security
+  private_endpoints_always_deployed = false           ## other option is: true
+  pii_data                          = "no"            ## other option is: unknown, yes. If yes, a whole bunch of security features will be turned on ($$)
+  phi_data                          = "no"            ## other option is: unknown, yes. If yes, a whole bunch of security features will be turned on ($$)
+
+  ## Tags
+  owner_service                     = "tbd"           ## freeform text, business owner  - email address
+  owner_tech                        = "tbd"           ## freeform text, technlogy owner - email address, this where alerts will go  
+  cost_centre                       = "unknown"       ## from the accountants, its the owner's cost centre. Freeform text
+  monitoring                        = "not-monitored" ## other options are: 24-7 or 8-5
+}
+
 ## Create an application landing zone, consisting of the following resources
 ## One Resource Group, One Public DNS Zone, Two User Assigned Identities, One Static Web App, One Log Analytics Workspace, plus 
 ## Application Insights (web), One KeyVault, One SQL Server associated to one SQL Server Elastic Pool (these are free, until you
